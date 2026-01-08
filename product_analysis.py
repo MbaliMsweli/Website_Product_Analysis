e30cac072f44ad1d6315f14f077a600b5f1aa6ec
Mbali Msweli <mbalimsweli.nongalo@gmail.com>
2026-01-08 18:27:38 +0200

    push

--- product_analysis.py
+++ product_analysis.py
@@ -1,8 +1,24 @@
 
 import pandas as pd
+#it lets your code work with folders and files and create filders automatically
+import os
 
 #Read WooCommerce CSV and understand that columns in csv are semi colon separated
 website_df = pd.read_csv("ab_nov_dec_sales2025.csv", sep=";")
+
+#Creates a folder called data/bronze
+os.makedirs("data/bronze", exist_ok=True)
+
+# Read raw WooCommerce export (bronze)
+bronze_df = pd.read_csv(
+    "ab_nov_dec_sales2025.csv", sep=";")
+
+# Save bronze layer (raw, untouched)
+bronze_df.to_csv(
+    "data/bronze/ab_nov_dec_sales2025_raw.csv",
+    index=False
+)
+print("Bronze dataset saved")
 
 
 #Print first 5 rows
@@ -53,3 +69,21 @@ website_df = website_df[
 
 #show the top 5 numbers 
 print(website_df["Phone Number"].head())
+
+#Creates a folder called data/silver
+os.makedirs("data/silver", exist_ok=True)
+
+#Save Cleaned Data to Silver Layer
+website_df.to_csv(
+    "data/silver/ab_nov_dec_sales2025_silver.csv",
+    index=False
+)
+
+print("Silver dataset created successfully")
+
+#Read the Silver dataset 
+silver_df = pd.read_csv("data/silver/ab_nov_dec_sales2025_silver.csv")
+
+#Preview Silver Dataset (First 5 Rows)
+print("\nFIRST 5 ROWS OF SILVER DATASET:")
+print(silver_df.head())
