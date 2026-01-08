2434648834489e05b6aec72566e413e06450594d
Mbali Msweli <mbalimsweli.nongalo@gmail.com>
2026-01-08 11:04:12 +0200

    push

--- product_analysis.py
+++ product_analysis.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 
 #Read WooCommerce CSV and understand that columns in csv are semi colon separated
@@ -31,3 +32,19 @@ website_df["Order Date"] = pd.to_datetime(website_df["Order Date"])
 #Print data types
 print("\nDATA TYPES:")
 print(website_df.dtypes)
+
+# Clean phone numbers to digits-only format
+website_df["Phone Number"] = (
+    website_df["Phone Number"]
+    .astype(str)
+    .str.replace(r"\D", "", regex=True)   # remove anything not a digit
+    .str.replace(r"^27", "", regex=True)  # remove country code 27 if it starts the number
+)
+
+# Keep only reasonable phone lengths (9 or 10 digits)
+website_df = website_df[
+    website_df["Phone Number"].str.len().isin([9, 10])
+]
+
+#show the top 5 numbers 
+print(website_df["Phone Number"].head())
